b83cda612e62f0ff56bceec83aa28428b8bb4309
Njalale <fnjalale@gmail.com>
2023-03-12 21:03:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const name = "Aki"
+
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}
+
+
+function introductionWithLanguage(name, language){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+let language = python
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
